a699f9b0810d68e9dfa5810b9531cfed1768bf64
Dombah <domagoj.leskovic24@gmail.com>
2024-01-24 10:06:26 +0100

    Register Quote in admin

--- contactacontractor/app/admin.py
+++ contactacontractor/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from app.models import Account, Job
+from app.models import Account, Job, Quote
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(Job)
+admin.site.register(Quote)
