ea8691786ccc578c3003bf7630ba689b04e0d207
Dombah <domagoj.leskovic24@gmail.com>
2024-01-24 10:19:04 +0100

    Register Message in admin

--- contactacontractor/app/admin.py
+++ contactacontractor/app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from app.models import Account, Job, Quote
-from app.models import Account, Job, Dispute
+from app.models import Account, Job, Quote, Dispute, Message
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(Job)
 admin.site.register(Quote)
 admin.site.register(Dispute)
+admin.site.register(Message)
+
