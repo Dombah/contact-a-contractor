e6579079a79de657ad5df2ba2d92eb197fc554d5
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-29 23:49:54 +0100

    add path for contractor dashboard

--- contactacontractor/contactacontractor/urls.py
+++ contactacontractor/contactacontractor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import home, register, user_dashboard, user_profile, new_job
+from app.views import home, register, user_dashboard, user_profile, new_job, contractor_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('accounts/register/', register, name='register'),
     path('account/user_dashboard/', user_dashboard, name='user_dashboard'),
     path('account/user_profile/', user_profile, name='user_profile'),
-    path('jobs/new', new_job, name='new_job')
+    path('jobs/new', new_job, name='new_job'),
+    path('account/contractor_dashboard/', contractor_dashboard, name='contractor_dashboard'),
 ]
