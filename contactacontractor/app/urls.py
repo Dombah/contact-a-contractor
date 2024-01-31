1f07910108887310a3357bd8f4951bedf833ed56
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-31 20:11:03 +0100

    change urls

--- contactacontractor/app/urls.py
+++ contactacontractor/app/urls.py
@@ -4,5 +4,5 @@ from .views import new_job, available_jobs, new_message
 urlpatterns = [
     path('new_job/', new_job, name='new_job'),
     path('available/', available_jobs, name='available_jobs'),
-    path('new_message/', new_message, name='new_message'),
+    path('new-message/', new_message, name='new_message'),
 ]
