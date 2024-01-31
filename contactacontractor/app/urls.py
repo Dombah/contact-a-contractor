197efaf022f8654c8e67be14029a4eef7e378832
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-31 21:05:37 +0100

    add path for new quote

--- contactacontractor/app/urls.py
+++ contactacontractor/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import new_job, available_jobs, new_message
+from .views import new_job, available_jobs, new_message, new_quote
 
 urlpatterns = [
     path('new_job/', new_job, name='new_job'),
     path('available/', available_jobs, name='available_jobs'),
     path('new-message/', new_message, name='new_message'),
+    path('new-quote/<int:job_id>/', new_quote, name='new_quote'),
 ]
