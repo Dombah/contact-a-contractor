cfb4dd5f391253c2498eb4941aea2e11153b4be1
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-31 19:03:46 +0100

    add string display for Message model

--- contactacontractor/app/models.py
+++ contactacontractor/app/models.py
@@ -39,6 +39,8 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
     text = models.TextField()
+    def __str__(self):
+        return f"{self.sender} to {self.receiver} - {self.text}"
     
 class Rating(models.Model):
     rater = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rater")
