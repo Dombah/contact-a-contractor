8207f02ac948bf6ef0d0174d583c9aa99249d43c
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-31 18:15:49 +0100

    add path for available jobs

--- contactacontractor/app/urls.py
+++ contactacontractor/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import new_job
+from .views import new_job, available_jobs
 
 urlpatterns = [
-    path('new/', new_job, name='new_job')
+    path('new/', new_job, name='new_job'),
+    path('available/', available_jobs, name='available_jobs'),
 ]
