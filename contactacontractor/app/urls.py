5d771ae03abf76d5aec60e83f490575067e0f23e
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2024-01-31 19:12:57 +0100

    add path to new message page

--- contactacontractor/app/urls.py
+++ contactacontractor/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import new_job, available_jobs
+from .views import new_job, available_jobs, new_message
 
 urlpatterns = [
-    path('new/', new_job, name='new_job'),
+    path('new_job/', new_job, name='new_job'),
     path('available/', available_jobs, name='available_jobs'),
+    path('new_message/', new_message, name='new_message'),
 ]
